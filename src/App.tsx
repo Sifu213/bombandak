f937e4d8837eebbacf90f8369ba6cc4b929436af
Sifu213 <sifu213@gmail.com>
2025-07-02 14:30:51 +0200

    og

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
               alt="Logo"
               className="w-8 h-8  sm:w-10 sm:h-10 lg:w-12 lg:h-12"
             />
-            Bobamdak
+            Bombandak
           </h1>
           <WalletConnect />
         </div>
